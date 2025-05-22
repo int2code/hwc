15e2a5d4f9ddd6c3c5019930982504645a429d68
Piotr Gałecki <piotr.galecki@bytesbakers.io>
2025-05-22 09:23:05 +0200

    Add set_engine() into Signals class

--- src/hwc/common/_base.py
+++ src/hwc/common/_base.py
@@ -119,6 +119,11 @@ class Signals(metaclass=SingletonMeta):
         """Set all updated signal states using given engine."""
         self._engine.write_states()
 
+    def set_engine(self, engine) -> None:
+        """Set engine instance."""
+        self._engine = engine
+        self._engine.associate_signal_members(self.__signal_members__)
+
     def __iter__(self):
         self._next_index = 0
         return self
